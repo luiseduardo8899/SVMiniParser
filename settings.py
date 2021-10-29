4ef217cb84bdb3d5aa0ef2525a2b73230943cb75
Marlon-Lazo-Coronado <mlazocoronado@hotmail.com>
2021-10-29 13:56:48 -0600

    SE actualiza el repo

--- settings.py
+++ settings.py
@@ -1,22 +1,3 @@
 #!/usr/bin/env python                                                                                                                                                                                      
   
 #Example:
-#FILENAME = "apciexactor-2.4a.cxl.json"
-#VIPPATH = "AVERY_PCIE"
-#PATHS = ["src", "src.VCS"]
-#FILTER = ["avery_defines.svh", "apci_phy_lane_rx.svh", "apci_phy_lane_tx.svh"]
- 
-FILENAME = "anvmexactor-2.8.json"
-VIPPATH = "AVERY_NVME"
-PATHS = ["src", "src.VCS"]
-FILTER = ["avery_defines.svh"]
- 
-#FILENAME = "apciexactor-2.4a.json"
-#VIPPATH = "AVERY_PCIE"
-#PATHS = ["src", "src.VCS"]
-#FILTER = ["avery_defines.svh", "apci_phy_lane_rx.svh", "apci_phy_lane_tx.svh"]
-
-#FILENAME = "onfi_sim.json"
-#VIPPATH = "AVERY_ONFI"
-#PATHS = ["src"]
-#FILTER = ["avery_defines.svh", "apci_phy_lane_rx.svh", "apci_phy_lane_tx.svh"]
