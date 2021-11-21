1a024456346f82ddd40c19e24e761d7b0ccf3e51
Marlon-Lazo-Coronado <mlazocoronado@hotmail.com>
2021-11-20 18:26:02 -0600

    Se agrega una versión que dispone de los datos del archivo excel, generalizado para tablas de 2 y 3 campos

--- script_registers/script_register.py
+++ script_registers/script_register.py
@@ -12,34 +12,32 @@ wb = load_workbook(filename = 'register_definition.xlsx')
 print(wb.sheetnames, '\n')
 sheets = wb.sheetnames
 
-
 for s in sheets:
     openfile = pd.read_excel(path, engine='openpyxl', sheet_name = s)
     data = openfile.values
     with open(s + '.sv', 'w') as f:
         for i in data:
-            if len(i) == 3:
-                copy = i[1]
-                str_space = copy.replace('(', '   (   ')
-                str_space = str_space.replace(')', '   )   ')
-                str_space = str_space.split()
+            copy = i[1]
+            str_space = copy.replace('(', '   (   ')
+            str_space = str_space.replace(')', '   )   ')
+            str_space = str_space.split()
+            print(str_space)
+            try:        
+                str_space = str_space[str_space.index('(') + 1:str_space.index(')')]
                 print(str_space)
-                try:        
-                    str_space = str_space[str_space.index('(') + 1:str_space.index(')')]
-                    print(str_space)
-                    base = str_space[0]
-                    offset = str_space[2]
-                except:
-                    str_space = ['none', 'none']
-                    base = str_space[0]
-                    offset = str_space[1]
-                
+                base = str_space[0]
+                offset = str_space[2]
+            except:
+                str_space = ['none', 'none']
+                base = str_space[0]
+                offset = str_space[1]
                 #print(str_space)
-
+            if len(i) == 3:
                 f.write('uvm _reg  ' + str(i[0]) + '  // ' + str(i[2]) + ', offset = (' + base + ' + ' + offset + ')')
                 f.write('\n')
             else:
-                print('suave')
+                f.write('uvm _reg  ' + str(i[0]) + '  // ' + str(i[1]) + ', offset = (' + base + ' + ' + offset + ')')
+                f.write('\n')
 
 
 '''for i in data:
--- settings.py
+++ settings.py
@@ -2,7 +2,7 @@
   
 #Example:
 FILENAME = "file.json"
-VIPPATH = "path"
+VIPPATH = "PATH"
 PATHS = ["src", "src.VCS"]
 FILTER = ["file.svh", "file.svh", "file.svh"]
  
