652248db9fc4dc32e7cd224914779b2b8ed9c3a1
Marlon-Lazo-Coronado <mlazocoronado@hotmail.com>
2021-12-01 09:55:41 -0800

    We added a version of the script that ignores the subsections of the .dox

---
+++ struct_reg_Ali/scrpt_read_docx.py
@@ -0,0 +1,85 @@
+import xlsxwriter  # build excel,   sudo apt-get install python3-xlsxwriter
+import docx
+
+#Created by Emmanuel Carballo Vargas, and Marlon L. Coronado.
+
+
+file_exc = xlsxwriter.Workbook('listed_info.xlsx')
+sheet = file_exc.add_worksheet()
+
+
+doc = docx.Document('prueba2.docx') 
+tablesD = doc.tables
+
+
+line_tex = [] #auxiliar lines vector to save info needed
+
+for i in range(len(doc.paragraphs)): 
+
+    line = doc.paragraphs[i].text #obtain the line text of the paragraph
+
+    line = line.split() #split using 'space' as parameter
+
+    #print(line)
+
+    if (len(line) == 2) and str(line[0])!='Table': #conditions for the line to be saved
+
+        line = [line[0], line[1]]
+        line_tex.append(line)
+
+
+#using as indexing the table
+i = 0  #index to run through rows of the exel...
+j = 0  #index to run through the 2nd and further columns
+k = 0  
+
+
+for table in tablesD:
+
+    #print('index = '+str(tablesD.index(table)))
+
+    if i==0: #build 1st row of titles for the exel (can be omptimized)
+        sheet.write(i, 0, 'Empty')
+        sheet.write(i, 1, 'Description Header')
+        sheet.write(i, 2, 'Bits')
+        sheet.write(i, 3, 'RW')
+        sheet.write(i, 4, 'Description')
+        sheet.write(i, 5, 'Default')
+
+        i=1
+
+        continue #continue with the other rows of the exel
+
+
+
+    if tablesD.index(table)%2!=0: #Skip pair (un desired) tables, instead of delete them.
+
+        if (len(table.rows) > 1): 
+
+            k = k + 1
+
+            sheet.write(i, 0, '-')
+            sheet.write(i, 1, line_tex[k-1][0])
+
+            noFirst = 0 #ommit the first row of every table...
+            for row in table.rows:
+            
+                if noFirst!=0:
+
+                    j = 0
+
+                    for cell in row.cells:
+
+                        print(cell.text)
+                        sheet.write(i, j+2, str(cell.text))
+
+                        j = j + 1
+
+                    i = i + 1
+
+                noFirst = noFirst+1
+
+file_exc.close()
+
+
+
